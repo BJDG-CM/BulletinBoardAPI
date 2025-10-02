722990dd34e4fb88cb0251f6f0a8f2b4ea708686
BJDG <yejuneric@gmail.com>
2025-10-02 11:28:51 +0900

    feat: Setup initial data structure and in-memory store

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { PostEntity } from './entities/post.entity';
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  private posts: PostEntity[] = [
+    {
+      id: 1,
+      userId: 'user-1',
+      title: '안녕하세요. 처음 뵙겠습니다.',
+      content: '반가워요',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      id: 2,
+      userId: 'user-1',
+      title: '점메추 부탁',
+      content: '점심 추천 받아용',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      id: 3,
+      userId: 'user-2',
+      title: '국밥 ㄱ',
+      content: 'ㅈㄱㄴ',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }
+  ];
+
+  private nextPostId = 3;
+  
+}
