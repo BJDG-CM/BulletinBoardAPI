e9458d3eeb33621dfe14e93939f4687f90ef9843
BJDG <yejuneric@gmail.com>
2025-10-09 17:27:55 +0900

    fix: Fix test files

--- src/posts/posts.controller.spec.ts
+++ src/posts/posts.controller.spec.ts
@@ -1,18 +1,36 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PostsController } from './posts.controller';
 import { PostsService } from './posts.service';
-import { PrismaService } from '../prisma/prisma.service';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 
 describe('PostsController', () => {
   let controller: PostsController;
+  let service: PostsService;
+
+  const mockPostsService = {
+    create: jest.fn(),
+    findAll: jest.fn(),
+    findOneById: jest.fn(),
+    update: jest.fn(),
+    remove: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PostsController],
-      providers: [PostsService, PrismaService],
-    }).compile();
+      providers: [
+        {
+          provide: PostsService,
+          useValue: mockPostsService,
+        },
+      ],
+    })
+      .overrideGuard(JwtAuthGuard)
+      .useValue({ canActivate: jest.fn(() => true) })
+      .compile();
 
     controller = module.get<PostsController>(PostsController);
+    service = module.get<PostsService>(PostsService);
   });
 
   it('should be defined', () => {
--- src/posts/posts.service.spec.ts
+++ src/posts/posts.service.spec.ts
@@ -1,15 +1,32 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PostsService } from './posts.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 describe('PostsService', () => {
   let service: PostsService;
+  let prisma: PrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PostsService],
+      providers: [
+        PostsService,
+        {
+          provide: PrismaService,
+          useValue: {
+            post: {
+              create: jest.fn(),
+              findMany: jest.fn(),
+              findUnique: jest.fn(),
+              update: jest.fn(),
+              delete: jest.fn(),
+            },
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<PostsService>(PostsService);
+    prisma = module.get<PrismaService>(PrismaService);
   });
 
   it('should be defined', () => {
